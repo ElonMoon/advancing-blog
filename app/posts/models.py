229be017f4e6eb1392bc7a63953474943411f9cb
kukka-joohyun <jh.mun@kukka.kr>
2019-03-25 16:17:19 +0900

    Post 기본MTV 작성

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='타이틀', max_length=128)
+    content = models.TextField(verbose_name='내용')
+    timestamp = models.DateTimeField(verbose_name='생성일자', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='수정일자', auto_now=True)
+
+    def __str__(self):
+        return self.title
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return HttpResponse('<p>포스트 리스트입니다</p>')
+
+
+def post_create(request):
+    return HttpResponse('<p>포스트 생성입니다</p>')
+
+
+def post_detail(request):
+    return HttpResponse('<p>포스트 하나입니다</p>')
+
+
+def post_update(request):
+    return HttpResponse('<p>포스트 수정입니다</p>')
+
+
+def post_delete(request):
+    return HttpResponse('<p>포스트 삭제입니다</p>')
