92535d80a50675c171e3e4f6f5f6d5a3aa047b57
kukka-joohyun <jh.mun@kukka.kr>
2019-03-26 11:46:43 +0900

    post update view, template 완성

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
@@ -15,6 +16,11 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, 'Successfully Created')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, 'Not Successfully Created')
+
     context = {
         'form': form,
     }
@@ -29,8 +35,18 @@ def post_detail(request, post_id=None):
     return render(request, 'post_detail.html', context)
 
 
-def post_update(request):
-    return HttpResponse('<p>포스트 수정입니다</p>')
+def post_update(request, post_id=None):
+    instance = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, '<a href="#">Item</a> Saved', extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
 
 
 def post_delete(request):
